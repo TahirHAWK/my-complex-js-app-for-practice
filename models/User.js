8612045fb99f16a0eb2ec2101b074cbcfe95da52
sumon <tahirtamin20@gmail.com>
2021-02-08 12:02:30 +0600

    connected to a database

--- models/User.js
+++ models/User.js
@@ -1,8 +1,28 @@
+const usersCollection = require('../db').collection("users")
 const validator = require("validator")
 
 let User = function (data) {
     this.data = data
     this.errors = []
+}
+
+User.prototype.cleanUp = function() {
+    if (typeof(this.data.username) != "string") {
+        this.data.username = ""
+    }
+    if (typeof(this.data.email) != "string") {
+        this.data.email = ""
+    }
+    if (typeof(this.data.password) != "string") {
+        this.data.password = ""
+    }
+
+    // get rid of any bogus properties
+    this.data = {
+        username: this.data.username.trim().toLowerCase(),
+        email: this.data.email.trim().toLowerCase(),
+        password: this.data.password
+    }
 }
 
 User.prototype.validate = function() {
@@ -31,9 +51,14 @@ User.prototype.validate = function() {
 
 User.prototype.register = function(){
     //step 1: validate user data
+    this.cleanUp()
     this.validate()
     //step 2: only if there are no validation errors
     //then save the user data into a database
+    if(!this.errors.length) {
+        usersCollection.insertOne(this.data) 
+    }
+
 
 }
 
