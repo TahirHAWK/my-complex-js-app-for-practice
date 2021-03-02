0c12e62fdfe79ac021ea856607060f5f567f9e5a
TahirHSTU <45144696+TahirHSTU@users.noreply.github.com>
2021-03-01 22:15:47 -0800

    added login and logout feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,15 +4,20 @@ exports.login = function(req, res) {
     let user = new User(req.body)
     user.login().then(function(result) {
         req.session.user = {favColor: "blue", username: user.data.username}
-        res.send(result)
+        req.session.save(function() {
+            res.redirect('/')
+        })
     }).catch(function(e){
         res.send(e)
     })
      
 }
 
-exports.logout = function() {
-
+exports.logout = function(req, res) {
+    req.session.destroy(function() {
+        res.redirect('/')
+    })
+    
 }
 
 exports.register = function(req, res) {
@@ -27,8 +32,10 @@ exports.register = function(req, res) {
 
 exports.home = function(req, res) {
     if(req.session.user) {
-        res.send("welcome to the actual application!! ")
+        res.render('home-dashboard', {username: req.session.user.username})
     } else {
         res.render('home-guest')
     }
 }
+
+
