b25443b3e2c88c53976454ff2626a397a820f811
TahirHSTU <45144696+TahirHSTU@users.noreply.github.com>
2021-03-13 23:29:29 -0800

    pulling in user data in post
    Pulled avatar and username

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
 const express = require('express')
-const { Cookie } = require('express-session')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
 const flash = require('connect-flash')
 const app = express()
 
 let sessionOptions = session({
-    secret: "JavaScript is so cool",
-    store: new MongoStore({client: require('./db')}),
-    resave: false,
-    saveUninitialized: false,
-    cookie: {maxAge: 1000 * 60 * 60 * 24, httpOnly: true}
-
+  secret: "JavaScript is sooooooooo coool",
+  store: new MongoStore({client: require('./db')}),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {maxAge: 1000 * 60 * 60 * 24, httpOnly: true}
 })
 
 app.use(sessionOptions)
 app.use(flash())
 
 app.use(function(req, res, next) {
-    res.locals.user = req.session.user
-    next()
+  res.locals.user = req.session.user
+  next()
 })
 
 const router = require('./router')
@@ -34,5 +32,3 @@ app.set('view engine', 'ejs')
 app.use('/', router)
 
 module.exports = app
-
-// {"cookie":{"originalMaxAge":86400000,"expires":"2021-03-03T23:53:59.121Z","httpOnly":true,"path":"/"},"flash":{"errors":["Please try again later."]}}
--- router.js
+++ router.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const userController = require('./controllers/userController')
 const postController = require('./controllers/postController')
-// user related routes
 
+// user related routes
 router.get('/', userController.home)
 router.post('/register', userController.register)
 router.post('/login', userController.login)
@@ -12,6 +12,6 @@ router.post('/logout', userController.logout)
 // post related routes
 router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
 router.post('/create-post', userController.mustBeLoggedIn, postController.create)
-
+router.get('/post/:id', postController.viewSingle)
 
 module.exports = router
