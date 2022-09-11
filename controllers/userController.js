e537171199db72a7073ee6f96948a590352eec00
TahirHSTU <tahirtamin20@gmail.com>
2022-09-11 09:40:45 +0600

    completed "what is a controller"
    start from "what is a model"

---
+++ controllers/userController.js
@@ -0,0 +1,14 @@
+exports.home = function(req, res){
+
+}
+
+
+
+exports.register = function(req, res){
+    res.send("thanks for trying to register")
+}
+
+
+exports.home = function(req, res){
+    res.render('home-guest')
+} 
