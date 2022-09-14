cdff76beb1ac8521cd2b9a96340631656cefb1e9
TahirHSTU <tahirtamin20@gmail.com>
2022-09-14 22:55:49 +0600

    completed quick misc cleanup
    start from "connecting to a database in a reusable fashion"

--- models/User.js
+++ models/User.js
@@ -5,6 +5,25 @@ let User = function(data){
     this.errors = []
 }
 
+User.prototype.cleanUp = function(){
+    if(typeof(this.data.username) != "string"){
+        this.data.username = ""
+    }
+    if(typeof(this.data.email) != "string"){
+        this.data.email = ""
+    }
+    if(typeof(this.data.password) != "string"){
+        this.data.password = ""
+    }
+
+    // get rid of any bogus properties
+    this.data = {
+        username: this.data.username.trim().toLowerCase(),
+        email: this.data.email.trim().toLowerCase(),
+        password: this.data.password
+    }
+}
+
 User.prototype.validate = function(){
     if(this.data.username == ""){
         this.errors.push("You must provide a valid username.")
@@ -36,8 +55,10 @@ User.prototype.validate = function(){
 
 User.prototype.register = function(){
     // validate user data
+    this.cleanUp()
     this.validate()
     // only if there are no validation errors then the user data into a database
+    
     
 }
 
